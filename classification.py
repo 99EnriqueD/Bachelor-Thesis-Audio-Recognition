84e2ec56b6d1cf9810294329fea8dbb09c580527
Timo <timomartens1999@gmail.com>
2020-03-10 18:16:18 +0100

    Preprocess other

--- classification.py
+++ classification.py
@@ -36,7 +36,7 @@ def get_data(path, sampleSize):
 
     #specificActivities = ['Calling', 'Clapping',
     #                      'Falling', 'Sweeping', 'WashingHand', 'WatchingTV']
-    specificActivities = ['GlassBreak', 'Scream', 'Crash', 'WaterSounds']
+    specificActivities = ['GlassBreak', 'Scream', 'Crash', 'WaterSounds', 'Other']
 
     enteringExiting = ['Entering', 'Exiting']
 
