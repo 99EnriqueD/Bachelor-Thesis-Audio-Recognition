51d007c813d4cc308c4ce6df889087bc253d2360
Enrique Dehaerne <99enriqued@gmail.com>
2020-05-02 20:16:46 +0200

    Move saveaudionpy

--- ALTpreprocessing.py
+++ ALTpreprocessing.py
@@ -9,20 +9,6 @@ Script that preprocesses wav files to npy files usable for the ML models
 This script does not use the STFT and is meant to be used for the ALTclassificationCONV.py models.
 """
 
-activities = ['Glassbreak', 'Scream',
-              'Crash', 'Other', 'Watersounds']
-
-subjects = ['s01', 's02', 's03', 's04', 's05']
-
-for activity in activities:
-    for subject in subjects:
-        innerDir = subject + "/" + activity
-        for file in os.listdir("Dataset_audio/" + innerDir):
-            if(file.endswith(".wav")):
-                save_audionpy("Dataset_audio/" + innerDir +
-                              "/" + file, file, activity, subject)
-                print(subject, activity, file)
-
 
 def save_audionpy(file, name, activity, subject):
     # read audio data
@@ -40,4 +26,19 @@ def save_audionpy(file, name, activity, subject):
     # save features
     np.save("NONSTFT_features/timeseries/" + subject + "_" +
             name[:-4] + "_" + activity + ".npy", trimmed)
+
+
+activities = ['Glassbreak', 'Scream',
+              'Crash', 'Other', 'Watersounds']
+
+subjects = ['s01', 's02', 's03', 's04', 's05']
+
+for activity in activities:
+    for subject in subjects:
+        innerDir = subject + "/" + activity
+        for file in os.listdir("Dataset_audio/" + innerDir):
+            if(file.endswith(".wav")):
+                save_audionpy("Dataset_audio/" + innerDir +
+                              "/" + file, file, activity, subject)
+                print(subject, activity, file)
 
